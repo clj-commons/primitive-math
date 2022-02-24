63dd00a01e0b05d3998500fb9076051f68ebf581
Erik Assum <erik@assum.net>
2022-02-24 21:07:15 +0100

    Setup for clj commons (#10)

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
-(defproject primitive-math "0.1.6"
+(defproject org.clj-commons/primitive-math
+    (or (System/getenv "PROJECT_VERSION") "0.1.6")
   :description "predictable, primitive math"
+  :url "https://github.com/clj-commons/primitive-math"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies []
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases true}]]
+
   :profiles {:dev {:dependencies [[criterium "0.4.1"]
                                   [org.clojure/clojure "1.8.0"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
@@ -12,4 +19,4 @@
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace ["-server"]
   :java-source-paths ["src"]
-  :javac-options ["-target" "1.5" "-source" "1.5"])
+  :javac-options ["-target" "1.8" "-source" "1.8"])
